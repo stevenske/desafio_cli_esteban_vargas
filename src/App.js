708c6ccc3dc752f1eef72e8f794f5ed5c345ec81
esteban vargas <esteban.var@outlook.com>
2022-08-25 19:09:48 -0300

    fix app.js classname tag

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <nav class="navbar bg-dark">
-        <div class="container-fluid d-flex justify-content-center">
-          <a class="navbar-brand text-white " href="index.html">
+      <nav className="navbar bg-dark">
+        <div className="container-fluid d-flex justify-content-center">
+          <a className="navbar-brand text-white " href="index.html">
             <img src={logo} alt="" width="30" height="24" className="App-logo d-inline-block align-text-top"/>
             A Tu Gusto
           </a>
